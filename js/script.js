c3ed44d0d8b8d379af2bb38bd4b7addefce00c04
Rang3rDang3r <stu-evans@live.co.uk>
2019-11-27 09:43:05 +0000

    added basic bar chart and div w/ id in index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
-    
+    var w = 500
+    var h= 500
+    var barPadding= 1;
+    var dataset= [100, 300, 450, 340, 230, 170, 480, 300, 200, 140];
 
+    var colWidth= w/dataset.length;
+    var barWidth= colWidth - barPadding;
+
+    var svg= d3.select("#draw-here")
+        .append ("svg")
+        .attr ("height", h)
+        .attr ("width", w)
+
+    svg.selectAll ("rect")
+        .data (dataset)
+        .enter ()
+        .append ("rect")
+        .attr ("x", function (d, i) {
+            return i * colWidth;
+        })
+        .attr ("y", function (d) {
+            return h - d;
+        })
+        .attr ("height", function (d) {
+            return d;
+        })
+        .attr ("width", barWidth);
+        
 })
